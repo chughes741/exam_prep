54c4c6c11358abc7c90ad58237b46885ebb0a3c4
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-09-08 21:49:15 -0400

    Made it pretty

--- ex02/lv01/repeat_alpha.c
+++ ex02/lv01/repeat_alpha.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-	int rep = 0;
-	if (argc != 2)
-		return (write(1, "\n", 1));
+	if (argc != 2) return (write(1, "\n", 1));
+
+	int repeat = 0;
 	for (int i = 0; argv[1][i]; ++i) {
-		if (argv[1][i] >= 65 && argv[1][i] <= 90) rep = argv[1][i] - 65;
-		if (argv[1][i] >= 97 && argv[1][i] <= 122) rep = argv[1][i] - 97;
-		while (rep--) write(1, &argv[1][i], 1);
+		if (argv[1][i] >= 65 && argv[1][i] <= 90) repeat = argv[1][i] - 65;
+		if (argv[1][i] >= 97 && argv[1][i] <= 122) repeat = argv[1][i] - 97;
+		while (repeat--) write(1, &argv[1][i], 1);
 		write(1, &argv[1][i], 1);
 	}
 	return 0;
