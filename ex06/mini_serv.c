f5a9ab334376964e5faa7a82101c38eb6adffb1d
chughes741 <95713039+chughes741@users.noreply.github.com>
2023-08-02 15:12:20 -0400

    Cleaned up ex06

--- ex06/mini_serv.c
+++ ex06/mini_serv.c
@@ -1,117 +1,105 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <errno.h>
 #include <string.h>
+#include <unistd.h>
+#include <netdb.h>
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define MAX_CLIENTS 1024
-#define MAX_BUFFER_SIZE 256
+#define MAX_BUFFER 512
+#define MAX_CLIENTS 256
 
-typedef struct {
-    int fd;
-    int id;
-} Client;
-
-void fatal_error(const char *msg) {
-    write(STDERR_FILENO, msg, strlen(msg));
-    write(STDERR_FILENO, "\n", 1);
-    exit(1);
+void fatal_error() {
+	write(STDERR_FILENO, "Fatal error\n", 12);
+	exit(1);
 }
 
-void send_to_all_clients(Client *clients, int num_clients, int sender_id, const char *message) {
-    char buffer[MAX_BUFFER_SIZE];
-    sprintf(buffer, MAX_BUFFER_SIZE, "client %d: %s", sender_id, message);
+void send_message(int *clients, const char *message, int client_id, bool from_server) {
+	char server_message[9] = "server: \0";
+	if (!from_server) {
+		server_message[0] = '\0';
+	}
 
-    for (int i = 0; i < num_clients; i++) {
-        if (clients[i].id != sender_id) {
-            if (send(clients[i].fd, buffer, strlen(buffer), 0) == -1) {
-                fatal_error("Fatal error");
-            }
-        }
-    }
+	char buffer[MAX_BUFFER + 64];
+	sprintf(buffer, "%sclient: %d %s", server_message, client_id + 1, message);
+	send(clients[client_id], buffer, strlen(buffer), 0);
 }
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fatal_error("Wrong number of arguments");
-    }
+	if (argc != 2) {
+		write(STDERR_FILENO, "Wrong number of arguments\n", 26);
+		exit(1);
+	}
 
-    int port = atoi(argv[1]);
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1) {
+		fatal_error("Fatal error");
+	}
 
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        fatal_error("Fatal error");
-    }
+	struct sockaddr_in server_addr = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = htonl(2130706433), //127.0.0.1
+		.sin_port = htons(atoi(argv[1]))
+	};
 
-    struct sockaddr_in server_addr = {0};
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
-    server_addr.sin_port = htons(port);
+	if (bind(server_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+		fatal_error("Fatal error");
+	} 
 
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
-        fatal_error("Fatal error");
-    }
+	if (listen(server_fd, 10) != 0) {
+		fatal_error("Fatal error");
+	}
 
-    if (listen(server_fd, 5) != 0) {
-        fatal_error("Fatal error");
-    }
-
-    Client clients[MAX_CLIENTS] = {0};
-    int num_clients = 0;
+	int clients[MAX_CLIENTS];
+	int num_clients = 0;
 
 	fd_set read_fds;
 	FD_ZERO(&read_fds);
 	FD_SET(server_fd, &read_fds);
-    int max_fd = server_fd;
+	int max_fd = server_fd;
 
-    while (1) {
-        if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
-            fatal_error("Fatal error");
-        }
+	while (1) {
+		if (select(max_fd, &read_fds, NULL, NULL, NULL) == -1) {
+			fatal_error("Fatal error");
+		}
 
-        if (FD_ISSET(server_fd, &read_fds)) {
-            int client_fd = accept(server_fd, NULL, NULL);
-            if (client_fd == -1) {
-                fatal_error("Fatal error");
-            } else {
-                clients[num_clients].fd = client_fd;
-                clients[num_clients].id = num_clients;
-                num_clients++;
-                FD_SET(client_fd, &read_fds);
-                max_fd = client_fd > max_fd ? client_fd : max_fd;
+		if (FD_ISSET(server_fd, &read_fds)) {
+			int client_fd = accept(server_fd, NULL, NULL);
+			if (client_fd == -1) {
+				fatal_error("Fatal error");
+			}
 
-                char welcome_message[MAX_BUFFER_SIZE];
-                sprintf(welcome_message, MAX_BUFFER_SIZE, "server: client %d just arrived\n", num_clients - 1);
-                send_to_all_clients(clients, num_clients, num_clients - 1, welcome_message);
-            }
-        }
+			clients[num_clients] = client_fd;
+			FD_SET(client_fd, &read_fds);
+			max_fd = client_fd > max_fd ? client_fd : max_fd;
 
-        for (int i = 0; i < num_clients; i++) {
-            if (FD_ISSET(clients[i].fd, &read_fds)) {
-                char buffer[MAX_BUFFER_SIZE];
-                int bytes_received = recv(clients[i].fd, buffer, MAX_BUFFER_SIZE - 1, 0);
+			send_message(clients, "just arrived\n", num_clients, true);
+			num_clients++;
+		}
 
-                if (bytes_received > 0) {
-                    buffer[bytes_received] = '\0';
-                    send_to_all_clients(clients, num_clients, clients[i].id, buffer);
+		for (int i = 0; i < num_clients; i++) {
+			if (FD_ISSET(clients[i], &read_fds)) {
+				char buffer[MAX_BUFFER];
+				int bytes_recieved = recv(clients[i], buffer, MAX_BUFFER - 1, 0);
 
-                } else {
-                    close(clients[i].fd);
+				if (bytes_recieved > 0) {
+					buffer[bytes_recieved] = '\0';
+					send_message(clients, buffer, i, false);
+				} else if (close(clients[i]) == -1) {
+					fatal_error("Fatal error");
+				} else {
+					send_message(clients, "just left\n", i, true);
 
-                    char message[MAX_BUFFER_SIZE];
-                    sprintf(message, MAX_BUFFER_SIZE, "server: client %d just left\n", clients[i].id);
-                    send_to_all_clients(clients, num_clients, clients[i].id, message);
-
-                    for (int j = i; j < num_clients - 1; j++) {
-                        clients[j] = clients[j + 1];
-                    }
-                    num_clients--;
-                }
-            }
-        }
-    }
-
-    return 0;
+					num_clients--;
+					for (int j = i; j < num_clients; j++) {
+						clients[j] = clients[j + 1];
+					}
+				}
+			}
+		}
+	}
+	return 0;
 }
