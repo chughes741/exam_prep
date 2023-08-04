7255209ccbffd25b65c4793930d1f981b4f223e6
Cole Hughes <coltenhughes@gmail.com>
2023-08-04 12:35:06 -0400

    Cleaned up mini_serv for readability

--- ex06/mini_serv.c
+++ ex06/mini_serv.c
@@ -9,12 +9,33 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+// If error happens, print error message and exit
 void fatal_error() {
 	write(STDERR_FILENO, "Fatal error\n", 12);
-	exit(1);
+	exit(EXIT_FAILURE);
 }
 
+// Allocate memory and exit if allocation fails
+void *xalloc(size_t nmemb, size_t size) {
+	void *ptr = calloc(nmemb, size);
+	if (ptr == NULL) {
+		fatal_error();
+	}
+	return ptr;
+}
+
+// Reallocate memory and exit if reallocation fails
+void *xrealloc(void *ptr, size_t nmemb, size_t size) {
+	ptr = realloc(ptr, nmemb * size);
+	if (ptr == NULL) {
+		fatal_error();
+	}
+	return ptr;
+}
+
+// Send a message to all clients except the sender
 void send_message_(int *clients, const char *message, int client_id, bool from_server) {
+	// Create prefix message
 	char server_message[64];
 	if (from_server) {
 		sprintf(server_message, "server: client %d ", client_id - 1);
@@ -22,30 +43,42 @@ void send_message_(int *clients, const char *message, int client_id, bool from_s
 		sprintf(server_message, "client %d: ", client_id - 1);
 	}
 
-	char *buffer = calloc(strlen(server_message) + strlen(message) + 1, sizeof(char));
-	if (buffer == NULL) fatal_error();
+	// Create buffer to store the message to be sent
+	char *buffer = xalloc(strlen(server_message) + strlen(message) + 2, sizeof(char));
 
+	// Copy the message to the buffer
 	sprintf(buffer, "%s%s\n", server_message, message);
+
+	// Send the message to all clients
 	for (int i = 1; i <= clients[0]; i++) {
 		if (i == client_id) continue;
 		if (send(clients[i], buffer, strlen(buffer), 0) == -1) fatal_error();
 	}
 
+	// Print the message
 	printf("%s", buffer);
 	free(buffer);
 }
 
-void send_message(int *clients, const char *message, int client_id, bool from_server) {
+void send_message(int *clients, char *message, int client_id, bool from_server) {
+	// If the message contains no newline characters, send the message as-is.
 	if (from_server || strstr(message, "\n") == NULL) {
 		send_message_(clients, message, client_id, from_server);
 	} else {
+		// Otherwise, send each part of the message separately.
 		char *head = (char *)message;
 		char *tail = strstr(message, "\n");
+
+		// Keep sending messages until there are no more newline characters in the message.
 		while (tail != NULL) {
-			char *buffer = calloc(tail - head + 1, sizeof(char));
-			if (buffer == NULL) fatal_error();
+			// Copy the part of the message before the newline into a new buffer.
+			char *buffer = xalloc(tail - head + 1, sizeof(char));
 			strncpy(buffer, head, tail - head);
+
+			// Send the part of the message before the newline.
 			send_message_(clients, buffer, client_id, from_server);
+
+			// Free the buffer and move on to the next part of the message.
 			free(buffer);
 			head = tail + 1;
 			tail = strstr(head, "\n");
@@ -59,22 +92,30 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
+	// Create socket
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) fatal_error();
+	if (server_fd == -1) {
+		fatal_error();
+	}
 
 	struct sockaddr_in server_addr = {
-		.sin_family = AF_INET,
-		.sin_addr.s_addr = htonl(2130706433), //127.0.0.1
-		.sin_port = htons(atoi(argv[1]))
+		.sin_family = AF_INET,					// IPv4
+		.sin_addr.s_addr = htonl(2130706433),	// 127.0.0.1
+		.sin_port = htons(atoi(argv[1]))		// Port
 	};
 
-	if (bind(server_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) fatal_error();
+	// Bind socket to port
+	if (bind(server_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+		fatal_error();
+	}
 
-	if (listen(server_fd, 10) != 0) fatal_error();
+	// Listen for connections
+	if (listen(server_fd, 10) != 0) {
+		fatal_error();
+	}
 
 	// clients[0] is the len of clients
-	int* clients = calloc(1, sizeof(int));
-	if (clients == NULL) fatal_error();
+	int* clients = xalloc(1, sizeof(int));
 	fd_set read_fds;
 	int max_fd = server_fd;
 
@@ -87,27 +128,24 @@ int main(int argc, char **argv) {
 		}
 
 		// Wait for activity
-		printf("\nWaiting for activity\n");
-		if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) fatal_error();
+		if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
+			fatal_error();
+		}
 
 		// Accept new connection
 		if (FD_ISSET(server_fd, &read_fds)) {
-			printf("Accepting new connection\n");
 			int client_fd = accept(server_fd, NULL, NULL);
-			if (client_fd == -1) fatal_error();
 
 			max_fd = client_fd > max_fd ? client_fd : max_fd;
 			clients[0] += 1;
-			clients = realloc(clients, (clients[0] + 1) * sizeof(int));
+			clients = xrealloc(clients, clients[0] + 1, sizeof(int));
 			clients[clients[0]] = client_fd;
-			if (clients == NULL) fatal_error();
 			send_message(clients, "just arrived", clients[0], true);
+
 		} else {
 			// Read message from client
 			for (int i = 1; i <= clients[0]; i++) {
-				printf("Checking client %d\n", i);
 				if (FD_ISSET(clients[i], &read_fds)) {
-					printf("Reading message from client\n");
 					char buffer[256];
 					bzero(buffer, 256);
 					int bytes_recieved = recv(clients[i], buffer, 255, 0);
@@ -121,13 +159,12 @@ int main(int argc, char **argv) {
 						for (int j = i; j <= clients[0]; j++) {
 							clients[j] = clients[j + 1];
 						}
-						clients = realloc(clients, (clients[0] + 1) * sizeof(int));
-						if (clients == NULL) fatal_error();
+						clients = xrealloc(clients, clients[0] + 1, sizeof(int));
 					}
 				}
 			}
 		}
 	}
 	free(clients);
-	return 0;
+	return EXIT_SUCCESS;
 }
